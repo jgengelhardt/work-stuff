50884f197e79e9ec580544322b471d963274792a
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2022-03-21 10:50:17 -0500

    Correct for pandas.append() deprecation
    Make generic and update for pandas.append() deprecation.

--- combine_all2.py
+++ combine_all2.py
@@ -6,13 +6,10 @@ files = os.listdir(cwd)
 # load chapter .csv files and append
 df = pd.DataFrame()
 source = pd.DataFrame()
+book = pd.DataFrame()
 for file in files:
-    if file.endswith('.csv'):
-        source = pd.read_csv(os.path.join(cwd, file), error_bad_lines=False)
-        source['Chapter'] = file.split()[0]
-        df = df.append(source) 
-df.rename({'Indian Name':'blank'}) # rename culturally insensitive, unused column
-df = df[ ['Chapter'] + [ col for col in df.columns] ]
-df['Year'] = 2014
-df = df[ ['Year'] + [ col for col in df.columns] ]
-df.to_csv('summer2014_istarCamps.csv')
+    if file.endswith('.xlsx'):
+        source = pd.concat(pd.read_excel(os.path.join(cwd, file),sheet_name=None))
+        df = pd.concat([df,source])
+        
+df.head()
